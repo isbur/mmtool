592a4261835bbb609b70a1fbb216047e68cf1c47
Igor B <isburenkov@gmail.com>
2019-06-12 21:20:09 +0300

    Научился отлаживать контент-скрипт

--- cscripts/selectioneer.js
+++ cscripts/selectioneer.js
@@ -1,13 +1,21 @@
+console.log("I am launched!");
+
+
+
 browser.runtime.onMessage.addListener(
 	function(request, sender, sendResponse){
+		console.log("I'm still alive!");
+		
 		if(request.command = "getSelection"){
 			console.log("I'm alive!");
 			var selObj = window.getSelection();
-			console.log(selObj);
+			console.log(selObj());
 			sendResponse({data: selObj.toString()});
 		} 
 		else {
 			sendResponse({});
 		}
+		
 	}
 )
+
