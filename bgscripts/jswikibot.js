8a9f28c65e859da9b10a4ee23f70af5407db5500
Igor B <isburenkov@gmail.com>
2019-06-16 03:30:20 +0300

    v0.4
     - finished jswikibot refactoring
     - finished and tested getPageText and find functions within jswikibot
     - improved typicalAjax
     - introduced path-browserify.posix (see browserify, nodejs and npm references) as path
     - rewrote three main commands: createCard, addText and test
     -- Now no conflicts with notification system
     -- Now able to add link to baseWikiPage while creating new card
     -- nested .then instead of await statements

--- bgscripts/jswikibot.js
+++ bgscripts/jswikibot.js
@@ -1,17 +1,34 @@
+import {path} from '../node_modules/path-browserify/ES6wrapper.js'
 import {apiUrl, userName, password, baseWikiPage} from './settings.js'
 
 
 function typicalAjax(requieredType, dataToSend){
-	return new Promise((resolve) => {
+	return new Promise((resolve, reject) => {
+		if ("format" in dataToSend === false) {
+			dataToSend.format = "json";
+		}
+		console.log(dataToSend);
 		$.ajax({
 			type: requieredType,
 			url: apiUrl,
 			data: dataToSend,
 			dataType: "json",
 			success: (data) => {
+				console.log("success!");
 				console.log(data);
 				resolve(data);
+			},
+			error: (response, type)=>{
+				console.log(response);
+				console.log(type);
+				throw "OK, so there was an error...";
+			},
+			
+			complete: (data) => {
+				console.log("Request completed...");
+				console.log(data);
 			}
+			
 		});
 	});
 }
@@ -38,37 +55,55 @@ export function login(){
 	
 
 // Needs to be refactored	
+// С другой стороны, работает - не тронь...
 export function editCard(cardName, textToAdd){
 		
 	return new Promise((resolve, reject) => {
-		$.ajax({
-			type: "GET",
-			url: apiUrl,
-			data: {action: "query", format: "json", meta: "tokens"},
-			dataType: "json",
-			success: function(data){
-				console.log(data);
-				var edittoken = data.query.tokens.csrftoken;
-				
-				var targetTitle = baseWikiPage.concat("",cardName);
-				
-				$.ajax({
-					type: "POST",
-					url: apiUrl,
-					data: {action: "edit", format: "json", title: targetTitle, appendtext: textToAdd, token: edittoken},
-					success: function(data){
-						console.log(data);
-					}	
-				})
-			}
+		let edittoken;
+		getToken("csrf")
+		.then((response)=>{
+			edittoken = response.query.tokens.csrftoken;
+			console.log(response);
 		})
+		.then(()=>{
+			let targetTitle = path.join(baseWikiPage,cardName);
+			let data = {action: "edit", title: targetTitle, appendtext: textToAdd, token: edittoken};
+			typicalAjax("POST", data);
+		})
+		.then((data)=>{
+			console.log(data);
+			resolve(data);
+		});
 	})
 }
 
 
 export function getPageText(cardName) {
 	return new Promise((resolve) => {
-		let data = {action: "parse", title: baseWikiPage+cardName, prop: "wikitext"}
-		resolve(typicalAjax("GET", data));
+		let targetPage = path.join(baseWikiPage,cardName);
+		//console.log(targetPage);
+		let data = {action: "parse", page: targetPage, prop: "wikitext"};
+		typicalAjax("GET", data)
+		.then((response)=>{
+			if ('error' in response) {
+				throw "Something went wrong...\n\nMaybe there is no such page?";
+			} else {
+				resolve(response);
+			}
+		})
 	});
+}
+
+
+export function find(textToFind, cardName) {
+	return new Promise((resolve) => {
+		console.log("Find Promise is queued");
+		// Get page's wikitext
+		getPageText(cardName) // no semicolon!!!
+		.then((response)=>{
+			let pageText = response.parse.wikitext['*']
+			console.log("textToFind:\t"+textToFind+"\nin\n"+pageText);
+			resolve(pageText.includes(textToFind));
+		});
+	})
 }
--- bgscripts/main.js
+++ bgscripts/main.js
@@ -1,60 +1,91 @@
-import './jquery-3.4.1.js'
 import {apiUrl, userName, password, baseWikiPage} from './settings.js'
+import {path} from '../node_modules/path-browserify/ES6wrapper.js'
 import * as jswikibot from './jswikibot.js'
 import * as browserbot from './browserbot.js'
 
 
-export var cardName
-async function universalCommandHandler(command){
-	let message = "default notification";
-	let textToAdd;
-	let response;
-	switch(command){
-		// Ctrl+Alt+T
-		case "testCommand":
-			console.log("test my nuts");
-			message = "test";
-			console.log("oh they're sweet");
-			break;
+export var cardName;
+function universalCommandHandler(command){
+	new Promise((resolve)=>{
+		let message = "default notification";
+		let targetTitle;
+		let textToAdd;
+		let textToFind;
+		switch(command){
+			// Ctrl+Alt+T
+			case "testCommand":
+				console.log("test my nuts");
+				message = "test";
+				
+				//browserbot.currentTab.getSelection()
+				//.then((data)=>{
+				//	let textToFind = data;
+				//	jswikibot.find(textToFind,"");
+				console.log("oh they're sweet");
+				resolve(message);
+				break;
+				
+			// Ctrl+Alt+V	
+			case "startCreatingCard":
 			
-		// Ctrl+Alt+V	
-		case "startCreatingCard":
-		
-		
-			// Set card name
-			cardName = await browserbot.currentTab.getSelection();
-			// Add card link to parent page
-			//// Form link text
-			let targetTitle = baseWikiPage + cardName
-			textToAdd = "\n\n[[" + targetTitle + "]]"
-			//// Check whether such link already exists
-			jswikibot.check
-			////// Get parent page's wikitext
-			//let parentPageWikitext = jswikibot.getPageText("");	// For some reason await disables notifications
-			jswikibot.getPageText("").then((parentPageWikitext)=>{
-				console.log("parentPageWikitext:\t"+parentPageWikitext);
-			})
-			//response = jswikibot.editCard("",)
-			message = "Successful card creation!\nCard path:\t"+baseWikiPage+cardName;
-			break;
-			
-		// Ctrl+Alt+M	
-		case "addSelectedText":
-			console.log("STARTED ADDING SELECTED TEXT");
-			textToAdd = await browserbot.currentTab.getSelection(); // defence from dumb input is needed to add
-			console.log("TEXTTOADD"+textToAdd);
-			response = jswikibot.editCard(cardName, "\n\n"+textToAdd+"\n\n");
-			console.log("RESPONSE");
-			console.log(response);
-			message = "Successful card edit!\nCard path:\t"+baseWikiPage+cardName+"\nText was added:\n"+textToAdd;
-			//console.log("MESSAGE"+message);
-			break;
-			
-		default:
-			console.log("Command was not found.");
-	}
-	//await browserbot.currentTab.notify(message);
-	browserbot.currentTab.notify(command, message);
+				// Set card name
+				browserbot.currentTab.getSelection()
+				// Add card link to parent page
+				//// Form link text
+				.then((data)=>{
+					cardName = data;
+					targetTitle = path.join(baseWikiPage, data);
+					textToFind = "[[" + targetTitle + "]]";
+					console.log(textToFind);
+					textToAdd = "\n\n[[" + targetTitle + "]]";
+					return textToFind;
+				})
+				//// Check whether such link already exists
+				.then((textToFind) => {
+					return jswikibot.find(textToFind,"");
+				})
+				.then((response)=>{
+					console.log(response);
+					if (response) {
+						console.log("Link on parent page already exists.");
+					} else {
+						jswikibot.editCard("",textToAdd);
+						console.log("Link successfully added");
+					}
+					message = "Successful card creation!\nCard path:\t"+targetTitle;
+					resolve(message);
+				})
+
+				break;
+				
+			// Ctrl+Alt+M	
+			case "addSelectedText":
+				console.log("STARTED ADDING SELECTED TEXT");
+				browserbot.currentTab.getSelection() // defence from dumb input is needed to add
+				.then((response)=>{
+					textToAdd = response;
+					console.log("TEXTTOADD"+textToAdd);
+					let returnValue = jswikibot.editCard(cardName, "\n\n"+textToAdd+"\n\n");
+					console.log("RETURN_VALUE:\t"+returnValue);
+					return returnValue.then((response)=>{return response}); // ну пошло костыляние
+				})
+				.then((response)=>{
+					console.log("FINISHED EDITING");
+					console.log(response);
+					message = "Successful card edit!\nCard path:\t"+path.join(baseWikiPage,cardName)+"\nText was added:\n"+textToAdd;
+					resolve(message);
+				});
+				
+				//console.log("MESSAGE"+message);
+				break;
+				
+			default:
+				console.log("Command was not found.");
+		}
+	})
+	.then((message)=>{
+		browserbot.currentTab.notify(command,message);
+	});
 }
 
 
